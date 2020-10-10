31aa7df59061439b72692b388de44b1edb0974a5
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2020-10-11 00:17:20 +0500

    completed pos page styling

--- src/pages/Pos/Pos.jsx
+++ src/pages/Pos/Pos.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Pos.scss';
+import './Cart.scss';
 import './ProductList.scss';
 import { Navbar } from '../../components';
-import { AiFillCalculator } from 'react-icons/ai';
+import { AiFillCalculator, AiOutlineClose } from 'react-icons/ai';
+import { MdRemoveShoppingCart, MdAdd, MdLocalShipping } from 'react-icons/md';
+import { BiMessageDetail } from 'react-icons/bi';
+import { RiPagesLine } from 'react-icons/ri';
 
 export default function Pos() {
   return (
@@ -46,11 +50,92 @@ export default function Pos() {
           </div>
         </article>
         {/* pos category section */}
-        <article className='categoryList'></article>
+        <article className='categoryListContainer'>
+          <div className='categoryList'>
+            <div className='productCategory category1'>
+              <div className='categoryImage image'></div>
+              <div className='categoryHeading'>
+                <div className='categoryTitle'>bakery</div>
+              </div>
+            </div>
+            <div className='productCategory category2'>
+              <div className='categoryImage'></div>
+              <div className='categoryHeading'>
+                <div className='categoryTitle'>bakery</div>
+              </div>
+            </div>
+            <div className='productCategory category3'>
+              <div className='categoryImage'></div>
+              <div className='categoryHeading'>
+                <div className='categoryTitle'>bakery</div>
+              </div>
+            </div>
+            <div className='productCategory category4'>
+              <div className='categoryImage'></div>
+              <div className='categoryHeading'>
+                <div className='categoryTitle'>bakery</div>
+              </div>
+            </div>
+          </div>
+        </article>
       </section>
 
       {/* cart section on right side */}
-      <section className='cart'>test</section>
+      <section className='cart'>
+        <div className='posTabs'>
+          <div className='cartButtons'>
+            <span className='active'>cart</span>
+            <span className='disabled'>Customer</span>
+            <span className='disabled'>Address</span>
+            <span className='disabled'>cart-img</span>
+          </div>
+        </div>
+        <div className='posItems'>
+          <div className='cartEmpty'>
+            <MdRemoveShoppingCart className='emptyImage' />
+            <span>Add the first product to the cart</span>
+          </div>
+        </div>
+        <div className='posTotals'>
+          <div className='cartTotal subtotal'>
+            <div className='totalLabel'>Subtotal</div>
+            <div className='totalPrice'>$0.00</div>
+          </div>
+          <div className='cartTotal totalTotal'>
+            <div className='totalLabel'>Subtotal</div>
+            <div className='totalPrice'>$0.00</div>
+          </div>
+        </div>
+        <div className='posActions'>
+          <div className='cartAction emptyCart'>
+            <AiOutlineClose size='1.3em' />
+            <span className='label'>EmptyCart</span>
+          </div>
+          <div className='cartAction addNote'>
+            <BiMessageDetail className='icon' size='1.5em' />
+            <span className='label'>Add Note</span>
+          </div>
+          <div className='cartAction discount'>
+            <MdAdd size='1.5em' className='icon' />
+            <span className='label'>Add fee or discount</span>
+          </div>
+          <div className='cartAction coupon'>
+            <RiPagesLine size='1.5em' className='icon' />
+            <span className='label'>Apply coupon</span>
+          </div>
+          <div className='cartAction shipping'>
+            <MdLocalShipping size='1.5em' className='icon' />
+            <span className='label'>Shipping</span>
+          </div>
+          <div className='cartAction save disabled'>
+            <MdRemoveShoppingCart size='1.5em' className='icon' />
+            <span className='label'>Suspend & save cart</span>
+          </div>
+          <div className='cartAction pay disabled'>
+            <span className='label'>Pay</span>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
