610d4baf89e9368dbd0c4ad03f6949e06846bc82
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2020-10-14 18:37:10 +0500

    Updates

---
+++ precache-manifest.f89d78917f2cde5c06660dde6a94c90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f971c7882457db6744fd9fa5125d31",
+    "url": "/point-of-sale/index.html"
+  },
+  {
+    "revision": "2661c6446f9cd8a39e49",
+    "url": "/point-of-sale/static/css/main.7a36f8ed.chunk.css"
+  },
+  {
+    "revision": "49dbe1759ededeae17e7",
+    "url": "/point-of-sale/static/js/2.a72970ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/point-of-sale/static/js/2.a72970ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2661c6446f9cd8a39e49",
+    "url": "/point-of-sale/static/js/main.da959e2c.chunk.js"
+  },
+  {
+    "revision": "c9e85923e49d07298fd3",
+    "url": "/point-of-sale/static/js/runtime-main.bca5b4d8.js"
+  }
+]);
