76dedafdbd31f4e8722100bc03f77a2faf3e71a1
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2020-10-15 12:09:24 +0500

    added customer form

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -7,12 +7,30 @@ export default function CustomerForm() {
       <div id='open-modal-customer' className='modal-window'>
         <div className='modal'>
           <div className='modal-header'>
-            <strong>Shipping Address</strong>
+            <strong>Set customer's profile</strong>
             <a href='#' title='Close' className='modal-close'>
               <strong>Close</strong>
             </a>
           </div>
-          <div className='modal-body'>body</div>
+          <div className='modal-body'>
+            <div className=''>Current Customer:</div>
+            <div className='current-customer'>
+              <span className='customer-name'>Guest</span>
+              <span className='search-icon'>search</span>
+            </div>
+            <input
+              type='search'
+              name=''
+              id=''
+              className='customer-form-search'
+            />
+            <span>
+              Please enter 3 or more characters.
+              <a href='/' className='customer-form-link'>
+                Create a new customer profile
+              </a>
+            </span>
+          </div>
         </div>
       </div>
     </>
