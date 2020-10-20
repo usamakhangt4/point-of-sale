6ee2349fb882e6bc9ac228c2fd515cc7fd11e766
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2020-10-20 22:53:13 +0500

    added calculator in edit items section

--- src/components/CartWithItems/CartWithItems.jsx
+++ src/components/CartWithItems/CartWithItems.jsx
@@ -2,35 +2,39 @@ import { AiOutlineClose } from 'react-icons/ai';
 import { MdEdit } from 'react-icons/md';
 import { BiMessageDetail } from 'react-icons/bi';
 import './CartWithItems.scss';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class CartWithItems extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isShowEdit: false
-    }
+      isShowEdit: false,
+    };
   }
 
   toggoleEdit = () => {
     this.setState({ isShowEdit: !this.state.isShowEdit });
-  }
+  };
 
   render() {
-    const { id, imgSRC, title, price, quantity, changeQuantityNumber, removeCartItem } = this.props;
+    const {
+      id,
+      imgSRC,
+      title,
+      price,
+      quantity,
+      changeQuantityNumber,
+      removeCartItem,
+    } = this.props;
     const { isShowEdit } = this.state;
     return (
       <article key={id}>
         {/* item row */}
         <div className='itemRow'>
           <span onClick={() => removeCartItem(id)}>
-          <AiOutlineClose className='removeIcon' size='1.5em' />
+            <AiOutlineClose className='removeIcon' size='1.5em' />
           </span>
-          <img
-            src={imgSRC}
-            alt=''
-            className='itemImage'
-          />
+          <img src={imgSRC} alt='' className='itemImage' />
           {/* <BiMessageDetail className='noteIcon' /> */}
           <div className='itemName'>
             <div className='nameTitle'>{title}</div>
@@ -40,7 +44,11 @@ export default class CartWithItems extends Component {
           </div>
           <div className='itemQuantity'>
             <div className='quantity'>
-              <div className='minus' onClick={() => changeQuantityNumber(id, 'decrease')}>-</div>
+              <div
+                className='minus'
+                onClick={() => changeQuantityNumber(id, 'decrease')}>
+                -
+              </div>
               <input
                 type='text'
                 className='quantityNumber'
@@ -48,37 +56,46 @@ export default class CartWithItems extends Component {
                 max=''
                 value={quantity}
               />
-              <div className='plus' onClick={() => changeQuantityNumber(id, 'increase')}>+</div>
+              <div
+                className='plus'
+                onClick={() => changeQuantityNumber(id, 'increase')}>
+                +
+              </div>
             </div>
           </div>
           <div className='itemPrice'>${price}</div>
-          <div className='itemTotal'>${(parseFloat(price) * quantity).toFixed(2)}</div>
+          <div className='itemTotal'>
+            ${(parseFloat(price) * quantity).toFixed(2)}
+          </div>
         </div>
 
         {/* item edit row` */}
-        {isShowEdit &&
+        {isShowEdit && (
           <div className='itemEdit'>
             <div className='editPrice'>
               <div className='priceLabel'>Change price per unit</div>
               <div className='priceInputWrapper'>
-                <input className='priceInput' readOnly='' value='16.00'></input>
+                <input className='priceInput' readOnly='' value='16.00' />
+                <div className='edit-item-calculator-wrapper show-calculator'>
+                  <div className='edit-item-calculator'>
+                    <h1>calculator here </h1>
+                  </div>
+                </div>
               </div>
             </div>
             <div className='editNote'>
               <div className='editNoteLabel'>
                 <BiMessageDetail className='noteIcon' />
-            Edit note
-          </div>
+                Edit note
+              </div>
               <textarea row='2'>sfsfsaf</textarea>
               <div className='editNoteButtonWrapper'>
                 <button className='updateButton'>Update</button>
               </div>
             </div>
           </div>
-        }
+        )}
       </article>
-
-    )
+    );
   }
 }
-
