81dccbbc62df1a11e1487b31d51010a0468c434c
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2020-10-23 00:41:36 +0500

    button styles

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import AuthService from './../../services/AuthService'
-import getAuthHeader from "./../../services/auth-header"
+import AuthService from './../../services/AuthService';
+import getAuthHeader from './../../services/auth-header';
 
 import './Login.scss';
 
@@ -10,79 +10,90 @@ export default class Login extends Component {
     this.state = {
       email: '',
       password: '',
-      isShowSelection: false
-    }
+      isShowSelection: false,
+    };
   }
 
   componentDidMount() {
     let authorizationHeader = getAuthHeader();
     if (authorizationHeader) {
-      AuthService.authorize(authorizationHeader).then(response => {
-        if(response.data.token && response.data.status === "success")
-        {
+      AuthService.authorize(authorizationHeader).then((response) => {
+        if (response.data.token && response.data.status === 'success') {
           this.props.history.push('/');
         }
-      })
+      });
     }
   }
 
   onEmailChange = (e) => {
     this.setState({ email: e.target.value });
-  }
+  };
 
   onPasswordChange = (e) => {
     this.setState({ password: e.target.value });
-  }
+  };
 
   handleLogin = () => {
     let loginObj = {
       password: this.state.password,
-      email: this.state.email
-    }
+      email: this.state.email,
+    };
     AuthService.login(loginObj)
-      .then(response => {
-        if(response.status === "success")
-        {
-          this.setState({isShowSelection: true})
+      .then((response) => {
+        if (response.status === 'success') {
+          this.setState({ isShowSelection: true });
         }
       })
-      .catch(err => {
-
-      })
-  }
+      .catch((err) => {});
+  };
 
   render() {
     return (
-      <section className="login-page">
+      <section className='login-page'>
         <div className='login-wrapper'>
-          {!this.state.isShowSelection ? 
-          <figure>
-            <img src='images/logo.jpeg' alt='logo' />
-            <h2>Login</h2>
-            <div className="login-form">
-              <input type='email' value={this.state.email} onChange={this.onEmailChange} />
-              <input type='password' value={this.state.password} onChange={this.onPasswordChange} />
-              <button onClick={this.handleLogin}>Login</button>
-            </div>
-          </figure> :
-          <figure>
-          <img src='images/logo.jpeg' alt='logo' />
-          <h2>Choose Store and Register</h2>
-          <form action='#' className='store-and-register-form'>
-            <select name='' id=''>
-              <option value=''>Choose a Store</option>
-              <option value=''>Grocery Store</option>
-              <option value=''>Clothing Store</option>
-            </select>
-            <select name='' id=''>
-              <option value=''>Choose a Register</option>
-              <option value=''>Evening Time Register</option>
-              <option value=''>Morning Time Register</option>
-            </select>
-          </form>
-          <button onClick={() => window.location.reload()}>Continue</button>
-        </figure>
-          }
+          {!this.state.isShowSelection ? (
+            <figure>
+              <img src='images/logo.jpeg' alt='logo' />
+              <h2>Login</h2>
+              <div className='login-form'>
+                <input
+                  type='email'
+                  value={this.state.email}
+                  onChange={this.onEmailChange}
+                />
+                <input
+                  type='password'
+                  value={this.state.password}
+                  onChange={this.onPasswordChange}
+                />
+                <button onClick={this.handleLogin} className='auth-card-button'>
+                  Login
+                </button>
+              </div>
+            </figure>
+          ) : (
+            <figure>
+              <img src='images/logo.jpeg' alt='logo' />
+              <h2>Choose Store and Register</h2>
+              <form action='#' className='store-and-register-form'>
+                <select name='' id=''>
+                  <option value=''>Choose a Store</option>
+                  <option value=''>Grocery Store</option>
+                  <option value=''>Clothing Store</option>
+                </select>
+                <select name='' id=''>
+                  <option value=''>Choose a Register</option>
+                  <option value=''>Evening Time Register</option>
+                  <option value=''>Morning Time Register</option>
+                </select>
+                <button
+                  onClick={() => window.location.reload()}
+                  className='auth-card-button'>
+                  Open Register
+                </button>
+              </form>
+            </figure>
+          )}
         </div>
       </section>
     );
